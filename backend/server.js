7a64741bafc1e7655826e8dd6d27d83b15381880
tobe <tobemarizu@gmail.com>
2025-01-03 17:03:47 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
@@ -20,7 +21,7 @@ cloudinary.config({
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve()
 app.use(express.json({limit:"5mb"})); //function that runs between req and res
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,6 +31,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 
